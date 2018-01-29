3324bd8d6908af34c1156bf41ee1ae332ee9ba42
PRANOYPC\Pranoy <pranoysantosh@gmail.com>
2018-01-29 17:26:17 -0500

    part1

---
+++ prototype.js
@@ -0,0 +1,60 @@
+var express = require('express'),
+	app = express(),
+	bodyParser  = require('body-parser'),
+	multer = require('multer'),
+	upload = multer({dest: 'uploads/'}),
+	fs = require('fs'),
+	PNF = require('google-libphonenumber'),
+	phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3000, ()=>{
+	console.log('Listening on PORT 3000')
+});
+
+app.get('/', (request, response) => {
+	response.status(200).send('Everything seems to be working!');
+});
+
+
+app.get('/api/phonenumbers/parse/text/:string', function(request, response) {
+	var stringArr = [];
+	stringArr.push(request.params.string);
+	var finalOutput = returnNumbers(stringArr, response);
+	response.send(finalOutput);
+});
+
+//https://www.npmjs.com/package/multer
+app.post('/api/phonenumbers/parse/file', upload.single('file'), function(require, response){
+	var buffer = fs.readFileSync(require.file.path),
+		string = buffer.toString('ascii'),
+		text = new Buffer(string, 'base64').toString('ascii'),
+		stringArr = text.split('\n'),
+		finalOutput = returnNumbers(stringArr, response);
+		response.send(finalOutput);
+});
+
+//uses npm package
+//https://www.npmjs.com/package/google-libphonenumber
+//read the input and return just the phone numbers.
+function returnNumbers(stringArr, response){
+	var numbers,
+		array = [];
+		
+	try{ //use try catch here to catch exceptions where no string is passed or even a string with no numbers is passed.
+		for (var i = 0; i < stringArr.length; i++){
+			numbers = phoneUtil.parse(stringArr[i], 'CA');
+			array.push(phoneUtil.format(numbers, PNF.INTERNATIONAL));				
+		}
+	}
+	catch(e){
+		response.status(400);
+	}
+
+	//https://www.codementor.io/tips/8243973127/how-to-remove-duplicates-within-a-javascript-array-using-es6-in-just-one-line	
+	var rmDuplicates = Array.from(new Set(array));
+
+	return rmDuplicates;
+}
