b359021d637f0477d9519f931a84d547cc0ffdb5
Soutrik Barua <sbarua91@gmail.com>
2018-02-22 10:33:40 -0500

    added word doc support

--- prototype.js
+++ prototype.js
@@ -6,16 +6,29 @@ var express = require('express'),
 	fs = require('fs'),
 	PNF = require('google-libphonenumber'),
 	phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+	var mammoth = require("mammoth");
 
+	
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
 app.listen(3000, ()=>{
-	console.log('Listening on PORT 3000')
+	console.log('Listening on PORT 3000');
+
 });
 
 app.get('/', (request, response) => {
-	response.status(200).send('Everything seems to be working!');
+	let text;
+	mammoth.extractRawText({path: "w1.docx"})
+    .then(function(result){
+        text = result.value; // The raw text
+		var messages = result.messages;
+		//console.log(text);
+		response.status(200).send('Everything seems to be working! ' + text);
+    })
+    .done();
+	
 });
 
 
@@ -58,6 +71,11 @@ function returnNumbers(stringArr, response){
 
 	return rmDuplicates;
 }
+
+
+
+
+
 
 /*
 https://stackoverflow.com/questions/33986863/mocha-api-testing-getting-typeerror-app-address-is-not-a-function
